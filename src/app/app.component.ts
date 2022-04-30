972e844b5f6221353308ba6a9584d13d5f034430
TamikoSRN <thomasereno1@gmail.com>
2022-04-30 07:51:48 -0300

    Project finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
+// Aca manejo justamente de donde sale el html que voy a trabajar y su estilo
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-practice';
-}
+// recomendable siempre agregar el type
+export class AppComponent {}
